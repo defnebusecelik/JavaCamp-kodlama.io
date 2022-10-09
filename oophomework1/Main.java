7702bf6e196aad45310c84f302d57aef1a1606c2
Defne Buse Çelik <110555559+defnebusecelik@users.noreply.github.com>
2022-10-09 22:59:24 +0300

    Create Main.java

---
+++ oophomework1/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Customer customer = new Customer(1,"Defne");
+	
+		customerManager CustomerManager = new customerManager(new Customer(),new MilitaryCreditManager());
+		CustomerManager.GiveCredit();
+		
+		Company company = new Company();
+		   company.setTaxNumber("1000");
+	    company.setAd("Arçelik");
+	    company.setId(2);
+	    
+	  customerManager cm2 = new customerManager(new Person(),new TeacherCreditManager());
+		
+		CreditManager creditManager = new CreditManager();
+        creditManager.Hesapla();
+        creditManager.Save();
+	}
+
+}
